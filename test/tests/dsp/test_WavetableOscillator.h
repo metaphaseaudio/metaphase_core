2d0112ef0e9e52a36fd63fffe1c2b97c9e1016c6
zappmatt <zappmatt@amazon.com>
2021-01-26 20:11:59 -0500

    adding option to run oscillator tests *really* long for profiling purposes

--- test/tests/dsp/test_WavetableOscillator.h
+++ test/tests/dsp/test_WavetableOscillator.h
@@ -24,17 +24,23 @@ public:
         meta::TestBase::initializeTestFile(meta::TestHelpers::testFolder.getChildFile(f));
     }
 
-    void runOscillator()
+    void runOscillator(int samples)
     {
-        juce::AudioBuffer<float> buffer(2, 4800);
-        buffer.clear();
-        for (int i = 0; i < 4800; i++)
+        constexpr int chunk_size = 10000;
+        for (int mod = samples / chunk_size; --mod >= 0;)
         {
-            auto sample = osc.tick();
-            buffer.setSample(0,i, sample);
+            const auto n_samps = std::min<int>(chunk_size, samples);
+            juce::AudioBuffer<float> buffer(1, n_samps);
+            buffer.clear();
+            for (int i = 0; i < n_samps; i++)
+            {
+                auto sample = osc.tick();
+                buffer.setSample(0,i, sample);
+            }
+            samples -= chunk_size;
+            m_Writer->writeFromAudioSampleBuffer(buffer, 0, buffer.getNumSamples());
         }
 
-        m_Writer->writeFromAudioSampleBuffer(buffer, 0, buffer.getNumSamples());
     }
 
     std::array<float, 1000> table = meta::BandlimitedWavetable<float, 1000>::makeSin();
@@ -46,12 +52,12 @@ TEST_F(WavetableOscillatorTest, generate_sin)
 {
 	osc.setFrequency(100);
     initializeTestFile("sin.wav");
-    runOscillator();
+    runOscillator(100000000);
 }
 
 TEST_F(WavetableOscillatorTest, generate_inverse_sin)
 {
 	osc.setFrequency(-100);
     initializeTestFile("inverted_sin.wav");
-    runOscillator();
+    runOscillator(10000000);
 }
