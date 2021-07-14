d42112b64886fb5adcbef7f14a1c979ddbf9ea10
Matt Zapp <info@metaphaseaudio.com>
2021-07-13 23:21:22 -0400

    iaagg: got the multi-chan convolve module working.

--- inc/meta/dsp/MultiChanConvolve.h
+++ inc/meta/dsp/MultiChanConvolve.h
@@ -15,11 +15,11 @@ namespace dsp
         MultiChanConvolve(juce::AudioBuffer<float>&& impulse, size_t max_block_size)
             : m_Spec{ 1, static_cast<juce::uint32>(max_block_size), 1 }
             , m_IRLength(impulse.getNumSamples())
-            , m_Conv(m_IRLength)
+            , m_Conv(impulse.getNumChannels())
+            , m_Tmp(1, m_IRLength + max_block_size)
         {
             for (auto c = impulse.getNumChannels(); --c >= 0;)
             {
-
                 juce::AudioBuffer<float> tmp(1, m_IRLength);
                 tmp.copyFrom(0, 0, impulse, c, 0, m_IRLength);
                 m_Conv.at(c).loadImpulseResponse(
@@ -31,31 +31,52 @@ namespace dsp
             }
         }
 
+        juce::AudioBuffer<float> convolve(juce::AudioBuffer<float>& in, bool include_tail=true)
+        {
+            auto total_length = in.getNumSamples() + (include_tail ? m_IRLength : 0);
+            juce::AudioBuffer<float> rv(m_Conv.size(), total_length);
+            juce::AudioBuffer<float> extended_in;
+            extended_in.makeCopyOf(in);
+            extended_in.setSize(in.getNumChannels(), total_length, true, true);
+            rv.clear();
+            juce::AudioBuffer<float> tmp_in, tmp_out;
+
+            for (int mod = 0; mod < total_length; mod += m_Spec.maximumBlockSize)
+            {
+                const auto proc_len = std::min<int>(m_Spec.maximumBlockSize, total_length - mod);
+                tmp_in.setDataToReferTo(extended_in.getArrayOfWritePointers(), in.getNumChannels(), mod, proc_len);
+                tmp_out.setDataToReferTo(rv.getArrayOfWritePointers(), rv.getNumChannels(), mod, proc_len);
+                process_block(tmp_in, tmp_out);
+            }
+
+            return rv;
+        }
+
         void process_block(juce::AudioBuffer<float>& in, juce::AudioBuffer<float>& out)
         {
-            const auto total_length = in.getNumSamples() + m_IRLength;
-            auto tmp = juce::AudioBuffer<float>(1, total_length);
+            jassert(in.getNumSamples() <= m_Spec.maximumBlockSize && out.getNumSamples() <= m_Spec.maximumBlockSize);
 
             for (auto chan = out.getNumChannels(); --chan >= 0;)
             {
                 // Clear temp
-                tmp.clear();
+                m_Tmp.clear();
 
                 // Convolve
                 juce::dsp::AudioBlock<float> in_block(in.getArrayOfWritePointers() + chan, 1, 0, in.getNumSamples());
-                juce::dsp::AudioBlock<float> out_block(tmp);
+                juce::dsp::AudioBlock<float> out_block(m_Tmp);
                 juce::dsp::ProcessContextNonReplacing<float> context(in_block, out_block);
                 m_Conv.at(chan).process(context);
 
                 // Move to output, trimmed
                 auto offset = 0;
-                out.copyFrom(chan, 0, tmp, 0, offset, total_length);
+                out.copyFrom(chan, 0, m_Tmp, 0, offset, in.getNumSamples());
             }
         }
 
     private:
-        juce::dsp::ProcessSpec m_Spec;
         int m_IRLength;
+        juce::dsp::ProcessSpec m_Spec;
+        juce::AudioBuffer<float> m_Tmp;
         std::vector<juce::dsp::Convolution> m_Conv;
     };
 }
--- test/tests/dsp/test_MultiChanConvolve.h
+++ test/tests/dsp/test_MultiChanConvolve.h
@@ -6,18 +6,20 @@
 #include <meta/dsp/MultiChanConvolve.h>
 
 
-TEST(MultiChanConvolveTest, introduce_delay)
+TEST(MultiChanConvolveTest, introduce_delay_multi_sub_block)
 {
-    juce::AudioBuffer<float> impulse(1, 2);
+    juce::AudioBuffer<float> impulse(2, 2);
     impulse.clear();
     impulse.setSample(0, 1, 1.0f);  // this should introduce a delay
+    impulse.setSample(1, 0, 1.0f);  // this should not
 
-    juce::AudioBuffer<float> signal(1, 5);
+    juce::AudioBuffer<float> signal(2, 5);
     signal.clear();
     signal.setSample(0, 4, 1.0f);
-    juce::AudioBuffer<float> output(1, impulse.getNumSamples() + signal.getNumSamples());
+    signal.setSample(1, 2, 1.0f);
+    juce::AudioBuffer<float> output(2, impulse.getNumSamples() + signal.getNumSamples());
 
-    meta::dsp::MultiChanConvolve convolve(std::move(impulse), 512);
-    convolve.process_block(signal, output);
+    meta::dsp::MultiChanConvolve convolve(std::move(impulse), 3);
+    output = convolve.convolve(signal, true);
     ASSERT_EQ(output.getSample(0, 5), 1.0f);
 }
