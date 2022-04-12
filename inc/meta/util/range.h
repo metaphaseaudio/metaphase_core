730acd0eed736dfa8b6942bbb0217b623c6f375b
Matt Zapp <info@metaphaseaudio.com>
2022-04-11 22:37:59 -0400

    range remap now doesn't have to use the same type. careful now.

--- inc/meta/util/range.h
+++ inc/meta/util/range.h
@@ -9,8 +9,8 @@
 
 namespace meta
 {
-    template <typename T>
-    T remap_range(const juce::Range<T>& out_range, const juce::Range<T>& in_range, const T& x)
+    template <typename T1, typename T2, typename T3>
+    T3 remap_range(const juce::Range<T1>& out_range, const juce::Range<T2>& in_range, const T3& x)
     {
         return out_range.getStart() + (x - in_range.getStart()) *
                (out_range.getEnd() - out_range.getStart()) / (in_range.getEnd() - in_range.getStart());
