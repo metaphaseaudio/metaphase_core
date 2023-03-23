dc076ef211d5def27582bd664f4754bcf37803b7
Matt Zapp <info@metaphaseaudio.com>
2023-03-23 09:09:03 -0400

    adding a filmstrip class to make creating filmstrip knobs a bit easier.

---
+++ inc/meta/gooey/Filmstrip.h
@@ -0,0 +1,22 @@
+//
+// Created by Matt on 3/19/2023.
+//
+
+#pragma once
+#include <juce_gui_basics/juce_gui_basics.h>
+#include "meta/util/enum/Orientation.h"
+
+namespace meta
+{
+    class Filmstrip
+    {
+    public:
+        Filmstrip(juce::Image* image, int sliceSizePx, meta::Orientation sliceOrientation);
+        [[nodiscard]] juce::Image getFrame(int index) const;
+
+    private:
+        const juce::Image* p_Img;
+        int m_SliceSizePx;
+        meta::Orientation m_Orient;
+    };
+}
---
+++ src/gooey/Filmstrip.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Matt on 3/19/2023.
+//
+
+#include "meta/gooey/Filmstrip.h"
+
+meta::Filmstrip::Filmstrip(juce::Image* image, int sliceSizePx, meta::Orientation sliceOrientation)
+    : p_Img(image)
+    , m_SliceSizePx(sliceSizePx)
+    , m_Orient(sliceOrientation)
+{
+    jassert(image != nullptr); // The image has to exist.
+    jassert(sliceSizePx >= 1);  // Slices must be at least 1px wide
+}
+
+juce::Image meta::Filmstrip::getFrame(int index) const
+{
+    // This will return an empty image so , but the caller should
+    jassert(index * m_SliceSizePx + m_SliceSizePx < (m_Orient == meta::Orientation::HORIZ ? p_Img->getWidth() : p_Img->getHeight()));
+
+    auto bounds = juce::Rectangle<int>(
+        m_Orient == meta::Orientation::HORIZ ? m_SliceSizePx * index : 0,
+        m_Orient == meta::Orientation::HORIZ ? 0 : m_SliceSizePx * index,
+        m_Orient == meta::Orientation::HORIZ ? m_SliceSizePx : p_Img->getWidth(),
+        m_Orient == meta::Orientation::HORIZ ? p_Img->getHeight() : m_SliceSizePx
+    );
+
+    return p_Img->getClippedImage(bounds);
+}
